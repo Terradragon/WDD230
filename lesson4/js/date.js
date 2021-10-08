a316e37e8d09dfaf8183a4febd961877b51b8427
terradragon <gof20001@byui.edu>
2021-10-08 17:45:36 -0400

    lesson 4 and lesson 2 editing

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -27,5 +27,5 @@ let d = new Date();
 let dayName = daynames[d.getDay()];
 let monthName = months[d.getMonth()];
 let year = d.getFullYear();
-let fulldate = dayName + ", " + d.getDate() + " " + monthName +", " + year;
+let fulldate = dayName + " " + d.getDate() + ", " + monthName +" " + year;
 document.getElementById("date").textContent = fulldate;
