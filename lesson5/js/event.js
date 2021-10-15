915ac5c6cab6723ff0f6f46b78766485a467481b
terradragon <gof20001@byui.edu>
2021-10-15 15:05:02 -0400

    Lesson 5 Weather Site 2.0

--- lesson5/js/event.js
+++ lesson5/js/event.js
@@ -1,13 +1,47 @@
-//add info//
-let day = new Date();
+/*add info
+let day = newDate();
 let week = d.getDay();
-
-//add message//
+.style.display = "block";
+add message
 let announcment;
-if(week == 3){
+if(week == 4){
     message = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pavilion";
 }
 else{
     message = "false";
 }
-document.getElementById("event").textContent= announcment;
+document.getElementById("event").textContent= announcment;*/
+/*function banner() {
+    let day = newDate();
+    let week = d.getDay();
+    let message = "";
+    if (week == 4) {
+      message = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pailion";
+    } else {
+      message = "false";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }*/
+
+function banner(){
+    let daynames = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let w =  daynames[d.getDay()];
+  let text;
+  /*text.style.bacgroundColor = "#954B93";
+  text.style.padding = "16px";*/
+  if (w == "Friday") {
+    text = "Saturday = Preston Pancakes in the Park! 9:00 a.m. Saturday at the city park pailion";
+  } else {
+    text = null;
+  }
+  
+  document.getElementById("demo").innerHTML = text;
+}
