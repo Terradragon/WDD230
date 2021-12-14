edae7dc0d29bcfc53d0c474306635122c5cb539a
terradragon <gof20001@byui.edu>
2021-12-13 21:34:07 -0500

    json 4th try

--- chamber-of-commerce/js/homedata.js
+++ chamber-of-commerce/js/homedata.js
@@ -1,30 +1,29 @@
-const requestURL = "https://raw.githubusercontent.com/Terradragon/wdd230/master/chamber-of-commerce/json/homedata.json"
+const requestURL = "https://raw.githubusercontent.com/Terradragon/wdd230/master/chamber-of-commerce/json/georgia.json"
+
 fetch(requestURL)
 .then(function(response){
     return response.json();
 })
 
-.then(function(jsonObject){
-    const company = (jsonObject)["company"];
-    for(let i = 0; i < company.length; i++){
+.then(function (jsObject){
+    const georgia = jsObject["georgia"]
+    for(let i = 0; i < georgia.length; i++){
         let card = document.createElement("section");
         let h2 = document.createElement("h2");
+        let logo = document.createElement("img");
         let p = document.createElement("p");
         let firstCol = document.createElement("column1");
         let secCol = document.createElement("column2");
-        let logo = document.createElement("img");
+        /*let div = document.createElement("div");*/
 
-        h2.textContent = company[i].name;
-        p.textContent = company[i].purpose;
-        logo.setAttribute("src", "images/" + company[i].logo);
-        logo.setAttribute("alt",company[i].name);
+        /*let companies = georgia.companies;
+        let allCompanies = document.createElement("div");*/
 
-        card.appendChild(firstCol);
-        card.appendChild(secCol);
-        firstCol.appendChild(h2);
-        firstCol.appendChild(p);
-        secCol.appendChild(logo);
+        h2.textContent = `${georgia.companies} Name`;
+        logo.setAttribute("src", "images/" + `${georgia.companies} logo`);
+        logo.setAttribute("alt", `${georgia.companies} Name`);
+        p.textContent = `${georgia.companies} Purpose`;
 
-        document.querySelector("div.cards_companies").appendChild(card);
+        document.querySelector(".cards_companies").appendChild(card);
     }
 })
