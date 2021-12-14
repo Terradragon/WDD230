804a9d4ba8ebae15b616ac48c178ae07ac76b138
terradragon <gof20001@byui.edu>
2021-12-14 12:11:16 -0500

    directory json fix again

--- chamber-of-commerce/js/directory.js
+++ chamber-of-commerce/js/directory.js
@@ -8,13 +8,24 @@ fetch(requestURL)
 .then(function(jsObject){
     const directory = jsObject["directory"]
     for(let i = 0; i < directory.length; i++){
+        let card = document.createElement("ul");
+        let name = document.createElement("li");
+        let own = document.createElement("p");
+        let add = document.createElement("p");
+        let phone = document.createElement("p");
+        let ms = document.createElement("p");
 
-                   document.getElementById("directory-table").innerHTML = `<tr>
+        name.textContent = directory[i].name;
+        own.textContent = directory[i].owner;
+        add.textContent = directory[i].address;
+        phone.textContent = directory[i].phone;
+        ms.textContent = directory[i].membersince;
+                   /*document.getElementById("directory-table").innerHTML = `<tr>
                    <td>${directory[i].name}</td>
                    <td>${directory[i].owner}</td>
                    <td>${directory[i].address}</td>
                    <td>${directory[i].phone}</td>
                    <td>${directory[i].membersince}</td>
-                  </tr>`
+                  </tr>`*/
     }
 })
