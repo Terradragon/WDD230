92f115e8dfde79a8454d1a1b1dd2d273adeaedb4
terradragon <gof20001@byui.edu>
2021-12-15 16:43:22 -0500

    website checkup/fixup

--- chamber-of-commerce/js/directory.js
+++ chamber-of-commerce/js/directory.js
@@ -4,28 +4,28 @@ fetch(requestURL)
 .then(function(response){
     return response.json();
 })
-
 .then(function(jsObject){
     const directory = jsObject["directory"]
     for(let i = 0; i < directory.length; i++){
-        let card = document.createElement("ul");
-        let name = document.createElement("li");
+        let card = document.createElement("section");
+        let name = document.createElement("h2");
         let own = document.createElement("p");
         let add = document.createElement("p");
         let phone = document.createElement("p");
         let ms = document.createElement("p");
+       
 
         name.textContent = directory[i].name;
-        own.textContent = directory[i].owner;
-        add.textContent = directory[i].address;
-        phone.textContent = directory[i].phone;
-        ms.textContent = directory[i].membersince;
-                   /*document.getElementById("directory-table").innerHTML = `<tr>
-                   <td>${directory[i].name}</td>
-                   <td>${directory[i].owner}</td>
-                   <td>${directory[i].address}</td>
-                   <td>${directory[i].phone}</td>
-                   <td>${directory[i].membersince}</td>
-                  </tr>`*/
+        own.textContent = "Owner: " + directory[i].owner;
+        add.textContent = "Address: " + directory[i].address;
+        phone.textContent = "Phone Number: " + directory[i].phone;
+        ms.textContent = "Member Since: " + directory[i].membersince;
+
+       card.appendChild(name);
+       card.appendChild(own);
+       card.appendChild(add);
+       card.appendChild(phone);
+       card.appendChild(ms);
+        document.querySelector(".directory-card").appendChild(card);
     }
 })
